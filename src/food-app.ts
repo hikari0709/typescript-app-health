160052ad2ec9edae06611abb91a0b1c430c5323f
hikari <tanimoto0709@gmail.com>
2022-09-10 16:27:42 +0900

    update: ファイルの読み込みパスの変更とクリックイベントの追加

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,3 +1,21 @@
-class Score { }
-class Food { }
-class Foods { }
+class Score {}
+
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    this.element.classList.toggle('food--active');
+  }
+}
+
+// Foodsは一欄だけを持っているclass
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  constructor() {
+    this.elements.forEach((element) => {
+      new Food(element);
+    });
+  }
+}
+const foods = new Foods();
